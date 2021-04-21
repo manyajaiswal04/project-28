30eede77b6d59d8b26598dafc0fcdfbc67ad88c2
manyajaiswal04 <72932927+manyajaiswal04@users.noreply.github.com>
2021-04-21 20:16:02 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,28 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.01,
+            length:10,
+        }
+        this.bodyA=bodyA;
+        this.pointB=pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world,this.launcher);
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    Launch(bodyA){
+        this.launcher.bodyA=bodyA;
+    }
+    display(){
+        if(this.launcher.bodyA){
+            var pointA=this.bodyA.position;
+            var pointB=this.pointB;
+            strokeWeight(3);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
